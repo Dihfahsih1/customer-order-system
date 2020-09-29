3186566cdb26b89f52bdd2e1410722678a519212
Dihfahsih1 <dihfahsihm@gmail.com>
2020-09-29 18:21:56 +0300

    create more tablle

--- accounts/models.py
+++ accounts/models.py
@@ -8,4 +8,31 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor','Indoor'),
+        ('Out Door','Out Door'),
+    )
+    name=models.CharField(max_length=200, null=True)
+    price=models.FloatField(null=True)
+    category=models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description=models.CharField(max_length=200, null=True)
+    date_created=models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
     
+class Order(models.Model):
+    STATUS = (
+        ('Pending','Pending'),
+        ('Out of delivery','Out of delivery'),
+        ('Delivered','Delivered'),
+    )
+    status=models.CharField(max_length=200, choices=STATUS, null=True)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    customer=models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    date_created=models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.customer
